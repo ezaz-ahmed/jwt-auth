3afd47d2b6211db82cabd721cae17d0dc50f1e88
Ezaz Ahmed <ezaz.dev@gmail.com>
2022-11-11 17:12:34 +0600

    ENV PORT fix

--- server/src/index.ts
+++ server/src/index.ts
@@ -7,9 +7,7 @@ import { json } from "body-parser";
 import { buildSchema } from "type-graphql";
 
 import { UserResolver } from "./UserResolver";
-
 import { AppDataSource } from "./data-source";
-// import { User } from "./entity/User";
 
 (async () => {
 	const app = express();
@@ -37,27 +35,7 @@ import { AppDataSource } from "./data-source";
 		}),
 	);
 
-	app.listen(4000, () => {
-		console.log("Express server is running at port: ", 4000);
+	app.listen(process.env.PORT, () => {
+		console.log(`Express server is running at port: ${process.env.PORT}`);
 	});
 })();
-
-// AppDataSource.initialize()
-// 	.then(async () => {
-// 		console.log("Inserting a new user into the database...");
-// 		const user = new User();
-// 		user.firstName = "Timber";
-// 		user.lastName = "Saw";
-// 		user.age = 25;
-// 		await AppDataSource.manager.save(user);
-// 		console.log(`Saved a new user with id: ${user.id}`);
-
-// 		console.log("Loading users from the database...");
-// 		const users = await AppDataSource.manager.find(User);
-// 		console.log("Loaded users: ", users);
-
-// 		console.log(
-// 			"Here you can setup and run express / fastify / any other framework.",
-// 		);
-// 	})
-// 	.catch((error) => console.log(error));
